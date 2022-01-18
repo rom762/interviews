1086c058f4a8c471d28064af44cf419bb676ce0e
nickr <nickro@ya.ru>
2022-01-19 01:00:49 +0300

    insert sort

--- insert_sort.py
+++ insert_sort.py
@@ -1,8 +1,44 @@
-a = [-3, 5, 0, -8, 1, 10]
+def insert_sort(a: list) -> list:
+    """
+    берем элемент сравниваем с предыдущим
+    если меньше меняем местами и так двигаемся от текущего элемента к нулю
+    потом берем следующий повторяем.
+    сложность n2? у меня получается что сложность n!
+    :param a:
+    :return:
+    """
+    counter = 0
+    for i in range(0, len(a)):
+        for j in range(i, 0, -1):
+            counter += 1
+            if a[j] < a[j-1]:
+                a[j], a[j-1] = a[j-1], a[j]
+            else:
+                break
 
-N = len(a)
+            print(counter, a)
+    return counter, a
 
-for i in range(1, N):
-    print(i)
-    for j in range(i, 0, -1):
-        print(f'____{j}')
+
+
+mylist = [
+    # [-3, 5, 0, -8, 1, 10, 7, 6, 9, 13]
+    # [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+    # [6, -1, 7, 6, 5, 4, 3, 2, 1, 0, 8],
+    # [i for i in range(11)],
+    [i for i in range(11, 0, -1)]
+
+    ]
+for each in mylist:
+    print(insert_sort(each))
+
+
+
+
+
+
+
+# mylist_sorted = list(reversed(sorted(mylist)))
+# print(mylist_sorted)
+# print(insert_sort(mylist))
+# print(insert_sort(mylist_sorted))
